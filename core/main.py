775a747f3e4e8d79df762edc779ba36fb99757e8
mmmhdp <malcevnik99@gmail.com>
2023-10-22 18:38:47 +0300

    prototype 2.0

--- core/main.py
+++ core/main.py
@@ -17,7 +17,6 @@ TOKEN = "6545388422:AAEewfhbnRhYSuHOyKxuxFzj6Ta5vFSf3Cw"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_keyboard = [["Как там сейчас на улице?"],
-                      ["А что там на недельке?"],
                       ["А как по погоде в городе N?"]]
     user = update.message.from_user
     user_name = user.full_name
@@ -33,30 +32,20 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def after_answer(update, context):
     reply_keyboard = [["Как там сейчас на улице?"],
-                      ["А что там на недельке?"],
                       ["А как по погоде в городе N?"]]
     await update.message.reply_text(
         f"Что ещё тебя интересует?",
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=False,
-            input_field_placeholder="А эта погода, она сейчас c нами в одной комнате?"
+            input_field_placeholder="Люблю запах \'облачно без просветлений\' по утру.."
         ),
     )
 
 
 async def get_current_location_for_current_info(update, context):
     await update.message.reply_text(
-        "а вы сейчас где находитесь?\n"
-        "если это не секрет, то отправьте мне свою геолокацию и я с удовольствием расскажу вам о погоде в вашем "
-        "регионе."
-    )
-    return 0
-
-
-async def get_current_location_for_week_info(update, context):
-    await update.message.reply_text(
-        "а вы сейчас где находитесь?\n"
-        "если это не секрет, то отправьте мне свою геолокацию и я с удовольствием расскажу вам о погоде в вашем "
+        "А вы сейчас где находитесь?\n"
+        "Если это не секрет, то отправьте мне свою геолокацию и я с удовольствием расскажу вам о погоде в вашем "
         "регионе."
     )
     return 0
@@ -66,13 +55,6 @@ async def get_location_and_show_weather(update, context):
     user_location = update.message.location
     w_api = WeatherApi()
     weather_info = w_api.get_weather_by_lat_and_lon(user_location.latitude, user_location.longitude)
-    await update.message.reply_text(weather_info)
-
-
-async def get_location_and_show_week_weather(update, context):
-    user_location = update.message.location
-    w_api = WeatherApi()
-    weather_info = w_api.get_weather_for_week(user_location.latitude, user_location.longitude)
     await update.message.reply_text(weather_info)
 
 
@@ -95,6 +77,7 @@ def main() -> None:
 
     application.add_handlers([
         CommandHandler("start", start),
+
     ])
 
     curr_day_conv_handler = ConversationHandler(
@@ -105,18 +88,9 @@ def main() -> None:
         states={
             0: [MessageHandler(filters.LOCATION, get_location_and_show_weather)]
         },
-        fallbacks=[CommandHandler("start", start)],
+        fallbacks=[MessageHandler(filters.TEXT, after_answer)]
     )
-    next_week_conv_handler = ConversationHandler(
-        allow_reentry=True,
-        entry_points=[
-            MessageHandler(filters.Regex("А что там на недельке?"), get_current_location_for_week_info),
-        ],
-        states={
-            0: [MessageHandler(filters.LOCATION, get_location_and_show_week_weather)],
-        },
-        fallbacks=[CommandHandler("start", start)]
-    )
+
     another_city_conv_handler = ConversationHandler(
         allow_reentry=True,
         entry_points=[
@@ -125,11 +99,10 @@ def main() -> None:
         states={
             0: [MessageHandler(filters.TEXT, get_city_name_and_show_weather)],
         },
-        fallbacks=[CommandHandler("start", start)]
+        fallbacks=[MessageHandler(filters.TEXT, after_answer)]
     )
 
     application.add_handler(curr_day_conv_handler)
-    application.add_handler(next_week_conv_handler)
     application.add_handler(another_city_conv_handler)
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
--- core/weather_api/api_call.py
+++ core/weather_api/api_call.py
@@ -1,4 +1,5 @@
 import json
+
 import requests
 
 
@@ -15,46 +16,11 @@ class WeatherApi:
         raw_info = result.text
         info = json.loads(raw_info)
 
-        custom_weather_description = (f"Вот текущая сводка по погоде:\n"
+        custom_weather_description = (f"Вот текущая сводка по погоде в {info['name']}:\n"
                                       f"\"{info['weather'][0]['description'].lower()}\"\n"
                                       f"Текущая температура {int(info['main']['temp'] - 273)}, но ощущается как {int(info['main']['feels_like'] - 273)}, всё по Цельсию\n"
                                       f"Ветер около {info['wind']['speed']} метров в секунду\n"
                                       f"Влажность около {info['main']['humidity']} процентов \n")
-        return custom_weather_description
-
-    def get_weather_for_week(self, lat, lon):
-        url = (f"http://api.openweathermap.org/data/2.5/forecast/daily?"
-               f"lat={lat}"
-               f"&lon={lon}&appid={self.__api_key}"
-               f"cnt=7"
-               f"&lang=ru"
-               )
-        result = requests.get(url=url)
-        raw_info = result.text
-        info = json.loads(raw_info)
-        try:
-            info_for_week = info["list"][:7]
-        except KeyError:
-            return "Так для такой инфы надо платную подписку на этот api иметь, вы чего..."
-        custom_weather_description = ""
-        for ind, info in enumerate(info_for_week):
-            match ind:
-                case 0:
-                    day = "сегодня"
-                case 1:
-                    day = "завтра"
-                case 2:
-                    day = "послезавтра"
-                case _:
-                    day = f"{ind} день после текущего"
-
-            custom_weather_description += (f"Вот текущая сводка по погоде на {day}:\n"
-                                           f"\"{info['weather'][0]['description'].lower()}\" \n"
-                                           f"Текущая температура {int(info['main']['temp'] - 273)},"
-                                           f" но ощущается как {int(info['main']['feels_like'] - 273)}, всё по Цельсию\n"
-                                           f"Ветер около {info['wind']['speed']} метров в секунду\n"
-                                           f"Влажность около {info['main']['humidity']} процентов \n")
-
         return custom_weather_description
 
     def get_weather_by_city(self, city):
@@ -65,6 +31,7 @@ class WeatherApi:
         result = requests.get(url=url)
         raw_info = result.text
         info = json.loads(raw_info)
+        print(info)
         try:
             lat, lon = info[0]["lat"], info[0]["lon"]
         except IndexError:
